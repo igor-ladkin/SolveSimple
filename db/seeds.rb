590f1812822ef7f4740ae1e981a6148aaa941e81
Igor Ladkin <dgkplan@googlemail.com>
2014-12-12 00:18:47 +0300

    Add application's layout, index and show views for question.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.destroy_all
+
+Question.create!([{
+	title: 'The most complicated question of all time.',
+	body: 'How is it possible to be so stupid?'
+},
+{
+	title: 'One of top ten unsolved problems in physics.',
+	body: 'What physics explains the enormous disparity between the gravitational scale and ' +
+				'the typical mass scale of the elementary particles? In other words, why is gravity ' +
+				'so much weaker than the other forces, like electromagnetism?'
+},
+{
+	title: 'Give me a cooking advice.',
+	body: 'Recently, I have started reading Rspec cooking book, but I do not understand ' +
+				'anything at all. How are you suppose to use these recepies? Do you bake them?'
+}])
+
+puts "Created #{Question.count} questions"
