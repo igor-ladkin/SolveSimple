20470e2ab082caff6ace4f4f899b1f38b5f2cbed
Igor Ladkin <dgkplan@googlemail.com>
2014-12-11 18:09:27 +0300

    Add POST, PATCH and DELETE methods to Questions controller.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -52,4 +52,96 @@ RSpec.describe QuestionsController, :type => :controller do
 			expect(response).to render_template :edit
 		end
 	end
+
+	describe 'POST #create' do
+		context 'with valid attributes' do
+			it 'saves the new question in the database' do
+				expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+			end
+
+			it 'redirects to questions#show' do
+				post :create, question: attributes_for(:question)
+				expect(response).to redirect_to question_path(assigns(:question))
+			end
+		end
+
+		context 'with invlid attributes' do
+			it 'does not save the new question in the database' do
+				expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+			end
+			it 're-renders the :new template' do
+				post :create, question: attributes_for(:invalid_question)
+				expect(response).to render_template :new
+			end
+		end
+	end
+
+	describe 'PATCH #update' do
+		context 'with valid attributes' do
+			it 'assigns requested question to @question' do
+				patch :update, id: question, question: attributes_for(:question)
+				expect(assigns(:question)).to eq question
+			end
+
+			it "changes @question's attributes" do
+				patch :update, id: question, question: { title: 'New Title', body: 'New body' }
+				question.reload
+				expect(question.title).to eq 'New Title'
+				expect(question.body).to eq 'New body'
+			end
+
+			it 'redirects to updated question' do
+				patch :update, id: question, question: attributes_for(:question)
+				expect(response).to redirect_to question
+			end
+		end
+
+		context 'with invalid attributes' do
+			let(:question) { create(:question, body: 'Some old body') }
+
+			before { patch :update, id: question, question: { title: 'New Title', body: nil } }
+
+			it "does not change @question's attributes" do
+				question.reload
+				expect(question.title).to_not eq 'New Title'
+				expect(question.body).to eq 'Some old body'
+			end
+
+			it 're-renders the :edit template' do
+				expect(response).to render_template :edit
+			end
+		end
+	end
+
+	describe 'DELETE #destroy' do
+		before { question }
+
+		it 'deletes the question from the database' do 
+			expect { delete :destroy, id: question }.to change(Question, :count).by(-1)
+		end
+		
+		it 'redirects to questions#index' do
+			delete :destroy, id: question
+			expect(response).to redirect_to questions_path
+		end
+	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :question do
     title { Faker::Hacker.say_something_smart }
 		body { Faker::Lorem.paragraph }
+
+		factory :invalid_question do
+			title nil
+		end
   end
 end
