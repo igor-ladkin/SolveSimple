6bd339346315193d9aed9d5ca0f47d155652df27
Igor Ladkin <dgkplan@googlemail.com>
2014-12-26 09:18:26 +0300

    Add attachment model.

---
+++ spec/features/attachments/add_file_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../feature_helper'
+
+feature 'Attach files to question', %q{
+	In order to give more details about the problem
+	As an author
+	I want to attach files
+} do
+	given!(:author) { create(:user) }
+	given(:question) { create(:question, user: author) }
+
+	background do
+		sign_in author
+		visit root_path
+		click_on 'Ask Question'
+	end
+
+	scenario 'User adds file when asks question' do
+		fill_in 'Title', with: question.title
+		fill_in 'Body', with: question.body
+		attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+		click_on 'Create Question'
+
+		expect(page).to have_content 'spec_helper.rb'
+	end
+end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,4 +9,5 @@ RSpec.describe Question, :type => :model do
   it { is_expected.to validate_presence_of :body }
   it { is_expected.to belong_to :user }
   it { is_expected.to have_many :answers }
+  it { is_expected.to have_many :attachments }
 end
